6ece73e0917cb016d6b2c8b0c8baac0a69879808
chenguojun <chenguojun@ouyeel.com>
2018-10-10 17:45:16 +0800

    m

---
+++ stream-server/src/main/java/com/nitewk/streamserver/StreamServerApplication.java
@@ -0,0 +1,12 @@
+package com.nitewk.streamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamServerApplication.class, args);
+    }
+}
--- stream-server/src/test/java/com/nitewk/streamserver/StreamServerApplicationTests.java
+++ stream-server/src/test/java/com/nitewk/streamserver/StreamServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nitewk.eurekaserver;
+package com.nitewk.streamserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class StreamServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
