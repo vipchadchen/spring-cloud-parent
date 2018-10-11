a445a4917397d7ae8756d7726a97b7b347dc15cd
chenguojun <chenguojun@ouyeel.com>
2018-10-11 09:33:46 +0800

    m

--- bus-server/src/main/java/com/nitewk/busserver/BusServerApplication.java
+++ bus-server/src/main/java/com/nitewk/busserver/BusServerApplication.java
@@ -1,12 +1,31 @@
 package com.nitewk.busserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+//这里面的属性有可能会更新的，git中的配置中心变化的话就要刷新，没有这个注解内，配置就不能及时更新
+@RefreshScope
 public class BusServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BusServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BusServerApplication.class, args);
+    }
+
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public String test(){
+        return this.name+this.age;
+    }
 }
